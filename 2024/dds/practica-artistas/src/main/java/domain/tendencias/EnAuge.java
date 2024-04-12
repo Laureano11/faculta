a77f5f62986645e858ad56fb08d3e03ca376be94
laureano11 <laureanoenrique29@gmail.com>
2024-04-11 22:17:44 -0300

    ahora si?

---
+++ 2024/dds/practica-artistas/src/main/java/domain/tendencias/EnAuge.java
@@ -0,0 +1,10 @@
+package domain.tendencias;
+
+import domain.catalogo.Cancion;
+
+public class EnAuge  implements Popularidad{
+    @Override
+    public String generarMensaje(Cancion cancion) {
+        return "";
+    }
+}
---
+++ 2024/dds/practica-artistas/src/main/java/domain/tendencias/EnTendencia.java
@@ -0,0 +1,10 @@
+package domain.tendencias;
+
+import domain.catalogo.Cancion;
+
+public class EnTendencia implements Popularidad {
+    @Override
+    public String generarMensaje(Cancion cancion) {
+        return "";
+    }
+}
